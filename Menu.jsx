c4309b90ae4f958194a799ef686353e8ebc57859
Ana Luiza Jesus Ferreira <163221130+Fejluiza@users.noreply.github.com>
2024-05-02 13:01:01 -0300

    React 02/05/2024

---
+++ Menu.jsx
@@ -0,0 +1,17 @@
+  import './Menu.css'
+
+  function Menu() {
+    return(
+        <>
+        <h2>Feira</h2>
+        <ul>
+        <li>Abacaxi</li>
+        <li>Abacate</li>
+        <li>Banana</li>
+        <li>Caqui</li>
+        <li>morango</li>
+        </ul>
+        </>
+    )    
+  }
+  export default Menu
---
+++ Tarefas.jsx
@@ -0,0 +1,16 @@
+
+function Menu() {
+  return(
+      <>
+      <h2>Tarefas</h2>
+      <ul>
+      <li></li>
+      <li>Abacate</li>
+      <li>Banana</li>
+      <li>Caqui</li>
+      <li>morango</li>
+      </ul>
+      </>
+  )    
+}
+export default Menu
